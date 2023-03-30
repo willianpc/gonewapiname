cf6a692000b96f07fd49e3cd3fb1260ef7535c31
Willian Carvalho <willian.carvalho@instana.com>
2023-03-30 14:38:39 +0200

    same functions for float32

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go some API
+
+...
--- gosomeapi.go
+++ gosomeapi.go
@@ -15,3 +15,19 @@ func Div(a, b int) int {
 func Mul(a, b int) int {
 	return a * b
 }
+
+func SumF(a, b float32) float32 {
+	return a + b
+}
+
+func SubF(a, b float32) float32 {
+	return a - b
+}
+
+func DivF(a, b float32) float32 {
+	return a / b
+}
+
+func MulF(a, b float32) float32 {
+	return a * b
+}
